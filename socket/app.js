4744fb21b59d80237e0c69051e918ceefc791491
Tina <tanjilla.tina@gmail.com>
2018-10-14 12:32:43 +0600

    Created views

--- socket/app.js
+++ socket/app.js
@@ -3,11 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const bodyParser=require('body-parser');
+const http=require('http');
+
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+//////*********creating server **************/
+
+const server=require('http').createServer(app);
+const io=require('socket.io').listen(server);
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,7 +28,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
+
+//defining routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -26,6 +40,7 @@ app.use('/users', usersRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -37,5 +52,10 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+///creating server
+
+
 
 module.exports = app;
